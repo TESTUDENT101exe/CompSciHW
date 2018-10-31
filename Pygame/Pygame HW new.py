1b56f100abf6fe1bdba9d4d8fa8422915d25158e
TESTUDENT101exe <43197347+TESTUDENT101exe@users.noreply.github.com>
2018-10-31 13:44:39 +0800

    Pygame HW

--- Pygame/Pygame HW new.py
+++ Pygame/Pygame HW new.py
@@ -1,35 +1,34 @@
-#Import Files
+
+#Import
 import pygame
 import random
 import time
 
 #Parts
-#moveX=[300]
-#moveY=[300]
 steps=0
-collision=False
 enddraw=False
 running=True
 failed=False
-screen = pygame.display.set_mode((600,600))
-screen.fill((255,255,255))
-objxy=[300,300]
+screen = pygame.display.set_mode((600 ,600))
+screen.fill((255 ,255 ,255))
+objxy=[300 ,300]
 complete_obstruction=0
-#newobjxy=[1,0]
 
+#Functions
 def obj():
-    pygame.draw.rect(screen, (0,0,0), (objxy, (1, 1)))
+    pygame.draw.rect(screen, (0 , 0, 0), (objxy, (1 , 1)))
+    
 def move():
-    global direct, collision, failed, complete_obstruction
+    global direct, failed, complete_obstruction
     runtime=0
     while runtime!=steps and failed!=True:
-        if direct==1 and screen.get_at((objxy[0]+1, objxy[1])) != (0,0,0):
+        if direct==1 and screen.get_at((objxy[0]+1, objxy[1])) != (0 ,0 ,0):
             objxy[0]+=1
-        elif direct==2 and screen.get_at((objxy[0], objxy[1]+1)) != (0,0,0):
+        elif direct==2 and screen.get_at((objxy[0], objxy[1]+1)) != (0 ,0 ,0):
             objxy[1]+=1
-        elif direct==3 and screen.get_at((objxy[0]-1, objxy[1])) != (0,0,0):
+        elif direct==3 and screen.get_at((objxy[0]-1, objxy[1])) != (0 ,0 ,0):
             objxy[0]=objxy[0]-1
-        elif direct==4 and screen.get_at((objxy[0], objxy[1]-1)) != (0,0,0):
+        elif direct==4 and screen.get_at((objxy[0], objxy[1]-1)) != (0 ,0 ,0):
             objxy[1]=objxy[1]-1
         else:
             complete_obstruction+=1
@@ -38,17 +37,19 @@ def move():
         print(objxy[0:])
         obj()
         pygame.display.update()
+        
 def direction():
     global failed, direct, complete_obstruction
     if failed==False:
         complete_obstruction=0
-        direct=random.randint(1,4)
+        direct=random.randint(1 ,4)
     elif failed==True:
         if direct==4:
             direct=1
         else:
             direct+=1
         failed=False
+        
 #Basic Code
 while running:
     for event in pygame.event.get():
@@ -57,7 +58,7 @@ while running:
 #Main Code
     while enddraw!=True:
         direction()
-        steps=random.randint(10,50)
+        steps=random.randint(10 ,50)
         time.sleep(0.1)
         move()
         if complete_obstruction==5:
